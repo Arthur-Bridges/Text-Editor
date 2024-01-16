aa4b5939711c1709530c886b35672933e2086f6b
Arthur Bridges <arthurbridges08@yahoo.com>
2024-01-15 18:02:07 -0600

    pseudo code with logic

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,19 @@
-const butInstall = document.getElementById('buttonInstall');
-
+const buttonInstall = document.getElementById("buttonInstall");
+//TODO: need a global variable that'll be use in multiple functions
+let;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  //TODO:save the 'event' in the global variable.
+  //TODO: render the button (buttonInstall).
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+buttonInstall.addEventListener("click", async () => {
+  //TODO: condition logic for if the button(buttonInstall) is clicked.
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  // TODO: logic updating the UI with a message.
+});
