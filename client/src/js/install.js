dc7c5ec22d50e26d732fc1a77b47564694d2fe15
Arthur Bridges <78990603+Arthur-Bridges@users.noreply.github.com>
2024-01-17 22:00:51 -0600

    Update install.js
    Removing TODOs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,32 +1,31 @@
 const buttonInstall = document.getElementById("buttonInstall");
-//TODO: need a global variable that'll be use in multiple functions
+// Need a global variable that'll be use in multiple functions
 let buttonState;
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-  //TODO:save the 'event' in the global variable.
+// save the 'event' in the global variable.
   buttonState = event;
-  //TODO: render the button (buttonInstall).
+  // render the button (buttonInstall).
   buttonInstall.style.display = "block";
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+Implement a click event handler on the `butInstall` element
 buttonInstall.addEventListener("click", async () => {
-  //TODO: condition logic for if the button(buttonInstall) is clicked.
+  // condition logic for if the button(buttonInstall) is clicked.
   if (buttonState) {
     buttonState.prompt();
     const { choice } = await buttonState.userChoice;
     console.log(`User choice: ${choice}`);
   } else {
-    //TODO: Disable the button by changing state.
+    // Disable the button by changing state.
     buttonState = null;
   }
-  //TODO: Make the button disappear after it's rendered.
+  // Make the button disappear after it's rendered.
   buttonInstall.style.display = "none";
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  // TODO: logic updating the UI with a message.
+  // logic updating the UI with a message.
   console.log('PWA has been installed');
 });
